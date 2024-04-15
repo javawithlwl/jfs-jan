fc0b9ff71a516a6ff54c69a54c3791537b6c1d43
javawithlwl <javawithlwl@gmail.com>
2024-04-15 08:16:38 +0530

    Added spring contact book application

---
+++ learning-spring-boot/src/main/java/com/careerit/sb/domain/Contact.java
@@ -0,0 +1,11 @@
+package com.careerit.sb.domain;
+
+import lombok.Data;
+
+@Data
+public class Contact {
+    private long id;
+    private String name;
+    private String mobile;
+    private String city;
+}
